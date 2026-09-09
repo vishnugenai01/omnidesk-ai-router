0fb0239c7232184a5d044d3566552782352043c8
Vishnu Vardhan <vishnugenai01@gmail.com>
2026-09-09 18:53:20 +0530

    initial commit

---
+++ config/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+llm = ChatGroq(
+    model="llama-3.1-8b-instant",
+    api_key=os.getenv("GROQ_API_KEY"),
+    temperature=0.3,
+)
