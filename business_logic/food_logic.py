b505648ff3354478541b44a18c2759f7956832d5
Vishnu Vardhan <vishnugenai01@gmail.com>
2026-09-21 10:50:02 +0530

    Fix restaurant list endpoint

--- business_logic/food_logic.py
+++ business_logic/food_logic.py
@@ -49,7 +49,7 @@ def list_restaurants() -> str:
     """
     try:
         response = requests.get(
-            f"{BASE_URL}/restaurants",
+            f"{BASE_URL}/restaurants/",
             timeout=60
         )
         
